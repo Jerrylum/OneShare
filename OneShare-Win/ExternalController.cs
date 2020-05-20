90e1cfe8a5c595f154473fd70ce0112bf8fcbc15
Jerrylum <serverofjerry@gmail.com>
2020-05-21 01:08:25 +0800

    加入專案檔案。

---
+++ OneShare-Win/ExternalController.cs
@@ -0,0 +1,26 @@
+﻿using EmbedIO;
+using EmbedIO.Routing;
+using EmbedIO.WebApi;
+using HttpMultipartParser;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace OneShare
+{
+    public class ExternalController : WebApiController
+    {
+        [Route(HttpVerbs.Post, "/input")]
+        public async Task<string> PostInput()
+        {
+
+            var parser = await MultipartFormDataParser.ParseAsync(Request.InputStream);
+            var content = parser.GetParameterValue("content");
+
+            API.SendString(content);
+
+            return "200 - OK";
+        }
+    }
+}
---
+++ OneShare-Win/Program.cs
@@ -0,0 +1,68 @@
+﻿
+namespace OneShare
+{
+    using System;
+    using System.Text;
+    using System.Threading;
+    using EmbedIO;
+    using EmbedIO.Files;
+    using EmbedIO.WebApi;
+
+
+    public class Program
+    {
+        private const string InternalUrl = "http://localhost:8080/";
+        private const string ExternalUrl = "http://+:8888/";
+
+
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            var internalServer = new WebServer(o => o
+                   .WithUrlPrefix(InternalUrl)
+                   .WithMode(HttpListenerMode.EmbedIO))
+               .WithStaticFolder("/", "internal", true);
+            ServerBasicSetup(internalServer);
+            internalServer.RunAsync();
+
+            var externalServer = new WebServer(o => o
+                   .WithUrlPrefix(ExternalUrl)
+                   .WithMode(HttpListenerMode.EmbedIO))
+               .WithStaticFolder("/", "external", true)
+               .WithWebApi("/api", m => m.WithController<ExternalController>());
+            ServerBasicSetup(externalServer);
+            externalServer.RunAsync();
+
+            //var browser = new System.Diagnostics.Process()
+            //{
+            //    StartInfo = new System.Diagnostics.ProcessStartInfo(InternalUrl) { UseShellExecute = true }
+            //};
+            //browser.Start();
+
+            Console.ReadKey(true);
+
+        }
+
+        private static void ServerBasicSetup(WebServer server)
+        {
+            server.HandleHttpException(async (ctx, ex) => {
+                ctx.Response.StatusCode = ex.StatusCode;
+
+                switch (ex.StatusCode)
+                {
+                    case 404:
+                        await ctx.SendStringAsync("404 Not Found", "text/html", Encoding.UTF8);
+                        break;
+                    default:
+                        // Handle other HTTP Status codes or call the default handler 'SendStandardHtmlAsync'
+                        await ctx.SendStandardHtmlAsync(ex.StatusCode);
+                        break;
+                }
+            });
+        }
+
+    }
+}
